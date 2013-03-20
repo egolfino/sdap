e236a948813b3af21b7c952ee364e40eed563f04
Elisa Golfinopoulos <egolfino@gmail.com>
2013-03-20 10:35:40 -0400

    testing the removal process

--- Audapter-DAF-64bitold/mcode/commonmcode/transAdaptDataDirBoucek.m
+++
@@ -1,3 +0,0 @@
-function d = transAdaptDataDirBoucek(varargin)
-    d = 'G:\CS_2004\ALL_DATA\';
-return
--- Audapter-DAF-64bitold/mcode/getSPLTarg2.m
+++
@@ -1,7 +0,0 @@
-function splt=getSPLTarg2(opt)
-    if (strcmp(opt,'prod')) % Transition prod
-        splt=78;
-    elseif (strcmp(opt,'perc'))
-        splt=72.5;
-    end
-return
